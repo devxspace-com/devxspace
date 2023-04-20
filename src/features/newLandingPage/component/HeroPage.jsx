75cca3c6cf67c1ad6f301ace66347a2b2fe25750
vicsnet <adesanmiadebisi@gmail.com>
2023-04-20 22:13:54 +0100

    hero fix 10%

--- src/features/newLandingPage/component/HeroPage.jsx
+++ src/features/newLandingPage/component/HeroPage.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Panel from "../../../images/office.png"
 import Image from "next/image";
 // import bg from '../../../images/homeBG.png';
-import bg from '../../../images/bg.jpeg';
+import bg from '../../../images/bg.jpg';
 import Mobilenav from '../../../components/Mobilenav';
 import Navbar from '../../../components/Navbar';
 import {useEffect, useState} from "react";
@@ -23,12 +23,12 @@ const HeroPage = () => {
 
   return (
     <main className=''>
-        <div className=" bg-center bg-cover bg-fixed bg-no-repeat h-screen w-full xsm:w-full sm:w-full opacity-[70%] " style={{backgroundImage: `url(${bg.src})`}}>
+        <div className=" bg-center bg-cover bg-fixed bg-no-repeat h-screen w-full xsm:w-full sm:w-full opacity-[] " style={{backgroundImage: `url(${bg.src})`}}>
         <div className='bg-[#000000]'>
         {isMobile ? <Mobilenav /> : <Navbar />}
         </div>
-        <div className='items-center h-[50%] w-[70%] sm:w-full lg:mt-52 xsm:mt-36 mx-auto'>
-        <h1 className='text-white font-extrabold lg:w-[80%] mx-auto text-center lg:text-center lg:text-[4.5rem] xsm:text-[3.5rem] xsm:text-left leading-[80px]' >Built to Enhance seamless Freelancing</h1>
+        <div className='items-center h-[50%] w-[70%] sm:w-full lg:mt-52 xsm:mt-36 mx-auto opacity-[100%]'>
+        <h1 className='text-[white] font-extrabold lg:w-[80%] mx-auto text-center lg:text-center lg:text-[4.5rem] xsm:text-[3.5rem] xsm:text-left leading-[80px]' >Built to Enhance seamless Freelancing</h1>
         <p className='text-[text-[#CCE9E7] w-[50%] xsm:w-[80%] lg:mx-auto xsm:mx-0 text-center items-center lg:text-center justify-center xsm:text-left mt-4'>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium</p>
         <button className='items-center flex justify-center text-[black] rounded-lg h-[55px] text-center w-[20%] xsm:w-[50%] lg:w-[20%] mx-auto mt-8 bg-[white] text-[18px] font-bold'>Get Started</button>
         </div>
