c45ece6de802f75c54a416f23f7acbd83960f167
akinbola247 <akinbolakehinde247@gmail.com>
2023-04-22 13:20:23 +0100

    latest

--- src/features/newLandingPage/component/ComingSoon.jsx
+++ src/features/newLandingPage/component/ComingSoon.jsx
@@ -18,7 +18,7 @@ Our goal is to build a space where developers can come together to exchange know
 </p>
             </div>
             <div className="w-[100%] ">
-                <Image src={hand} alt='community dao' className='w-[100%] absolute right-0 h-[570px] smDesktop:h-[694px] smDesk:h-[760px] tabletAir:top-0 tabletAir:h-[660px] mobile:h-[566px] opacity-[50%]'/>
+                <Image src={hand} alt='community dao' className='w-[100%] absolute right-0 h-[600px] smDesktop:h-[694px] smDesk:h-[760px] tabletAir:top-0 tabletAir:h-[660px] mobile:h-[566px] opacity-[50%]'/>
             </div>
         </section>
         </main>
